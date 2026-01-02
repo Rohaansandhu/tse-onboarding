85058e309a9b405ca2516bec08d5dcfd6ec12982
Rohaansandhu <rohaan.sandhu@gmail.com>
2026-01-02 11:34:43 -0800

    format backend

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -6,10 +6,10 @@ import type { RequestHandler } from "express";
 export const getAllTasks: RequestHandler = async (req, res, next) => {
   try {
     // your code here
-    const tasks = await TaskModel.find().sort({ dateCreated: -1});
+    const tasks = await TaskModel.find().sort({ dateCreated: -1 });
 
     if (tasks === null) {
-        throw createHttpError(404, "Task not found.");
+      throw createHttpError(404, "Task not found.");
     }
 
     res.status(200).json(tasks);
